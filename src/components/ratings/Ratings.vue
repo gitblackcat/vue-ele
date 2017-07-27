4a78fe64bb0d35d88dfe5a834060426956000267
gitblackcat <907258244@qq.com>
2017-07-27 20:42:06 +0800

    update

--- src/components/ratings/Ratings.vue
+++ src/components/ratings/Ratings.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ratings">
+    <div class="ratings" ref="ratings">
         <div class="ratings-content">
             <div class="overview">
                 <div class="overview-left">
@@ -24,11 +24,44 @@
                     </div>
                 </div>
             </div>
+            <vSplit></vSplit>
+            <vRating :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="ratings" @ratingType="ratingType" @contentToggle="contentToggle"></vRating>
+            <div class="rating-wrapper">
+                <ul>
+                    <li v-show="needShow(rating.rateType,rating.text)" class="rating-item border-1px" v-for="rating in ratings">
+                        <div class="avatar">
+                            <img :src="rating.avatar" width="28" height="28">
+                        </div>
+                        <div class="content">
+                            <h1 class="name">{{rating.username}}</h1>
+                            <div class="star-wrapper">
+                                <vStar :size="24" :score="rating.score"></vStar>
+                                <span class="delivery" v-if="rating.delivery">{{rating.deliveryTime}}</span>
+                            </div>
+                            <p class="text">{{rating.text}}</p>
+                            <div class="recommend" v-if="rating.recommend && rating.recommend.length">
+                                <span class="icon-thumb_up"></span>
+                                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+                            </div>
+                            <div class="time">{{rating.rateTime | formatDate}}</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
 <script>
+import bScroll from 'better-scroll'
 import vStar from '../star/star.vue'
+import vSplit from '../split/Split.vue'
+import vRating from '../ratingSelect/RatingSelect.vue'
+import { FormatDate } from '../../common/js/date.js'
+
+const POSITIVE = 0,
+    NEGATIVE = 1,
+    ALL = 2,
+    ERR_OK = 0
 
 export default {
     props: {
@@ -36,12 +69,71 @@ export default {
             type: Object
         }
     },
+    created() {
+        this.$http.get('https://www.easy-mock.com/mock/5935507491470c0ac1018212/vue-ele/vue-ele/ratings').then((res) => {
+            let data = res.data
+            if (data.errno === ERR_OK) {
+                this.ratings = data.data
+                this.$nextTick(() => {
+                    this.scroll = new bScroll(this.$refs.ratings, {
+                        click: true
+                    })
+                })
+            }
+        })
+    },
+    data() {
+        return {
+            ratings: [],
+            selectType: ALL,
+            onlyContent: true,
+            desc: {
+                all: "全部",
+                positive: "满意",
+                negative: "不满意"
+            }
+        }
+    },
+    methods: {
+        needShow(type, text) {
+            if (this.onlyContent && !text) {
+                return false
+            }
+            if (this.selectType === ALL) {
+                return true
+            } else {
+                return type === this.selectType
+            }
+        },
+        ratingType(type) {
+            this.selectType = type
+            this.$nextTick(() => {
+                this.scroll.refresh();
+            })
+        },
+        contentToggle(onlyContent) {
+            this.onlyContent = onlyContent
+            this.$nextTick(() => {
+                this.scroll.refresh();
+            })
+        }
+    },
+    filters: {
+        formatDate(time) {
+            let date = new Date(time)
+            return FormatDate(date, "yyyy-MM-dd hh:mm")
+        }
+    },
     components: {
-        vStar
+        vStar,
+        vSplit,
+        vRating
     }
 }
 </script>
 <style lang="stylus">
+@import "../../common/stylus/mixin.styl"
+
     .ratings
         position absolute
         top 174px
@@ -104,4 +196,64 @@ export default {
                         color rgb(147,153,159)
                         font-size 12px
                         margin-left 12px
+        .rating-wrapper
+            padding: 0 18px
+            .rating-item
+                display: flex
+                padding: 18px 0
+                border-1px(rgba(7, 17, 27, 0.1))
+                .avatar
+                    flex 0 0 28px
+                    width 28px
+                    margin-right 12px
+                    img
+                        border-radius 50%
+                .content
+                    flex auto
+                    position relative
+                    .name
+                        margin-bottom: 4px
+                        line-height: 12px
+                        font-size: 10px
+                        color: rgb(7, 17, 27)
+                    .star-wrapper
+                        margin-bottom: 6px
+                        font-size: 0
+                        .star
+                            display: inline-block
+                            margin-right: 6px
+                            vertical-align: top
+                        .delivery
+                            display: inline-block
+                            vertical-align: top
+                            line-height: 12px
+                            font-size: 10px
+                            color: rgb(147, 153, 159)
+                    .text
+                        margin-bottom: 8px
+                        line-height: 18px
+                        color: rgb(7, 17, 27)
+                        font-size: 12px
+                    .recommend
+                        line-height: 16px
+                        font-size: 0
+                        .icon-thumb_up, .item
+                            display: inline-block
+                            margin: 0 8px 4px 0
+                            font-size: 9px
+                        .icon-thumb_up
+                            color: rgb(0, 160, 220)
+                        .item
+                            padding: 0 6px
+                            border: 1px solid rgba(7, 17, 27, 0.1)
+                            border-radius: 1px
+                            color: rgb(147, 153, 159)
+                            background: #fff
+                    .time
+                        position: absolute
+                        top: 0
+                        right: 0
+                        line-height: 12px
+                        font-size: 10px
+                        color: rgb(147, 153, 159)
 </style>
